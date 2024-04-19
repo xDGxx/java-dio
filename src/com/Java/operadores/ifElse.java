dfc800e1a2f6662c57e1a070cf9a61ad5d2166c5
xDGxx <cntt.dg@gmail.com>
2024-04-18 23:45:04 -0300

    new commit

---
+++ src/com/Java/operadores/ifElse.java
@@ -0,0 +1,21 @@
+package com.Java.operadores;
+
+public class ifElse {
+    public static void main(String[] args) {
+        int a,b;
+        a = 7;
+        b  = 9;
+
+        /*String resultado = "";
+        if(a==b) {
+            resultado = "verdadeiro";
+        }else{
+                resultado = "falso";
+            System.out.println(resultado);
+            }
+         */
+
+        String resultado =  a==b ? "verdadeiro" : "falso"; //if = ? ||| //else = :
+        System.out.println(resultado);
+    }
+}
